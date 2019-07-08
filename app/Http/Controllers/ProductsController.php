37c14d1818d077dee8b97a771e555c1403c42b1e
Hamza <hamzabitup@gmail.com>
2019-07-08 23:55:54 +0100

    made the language change when requesting over opencart

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Cookie\CookieJar;
 
 class ProductsController extends Controller
 {
@@ -10,9 +11,13 @@ class ProductsController extends Controller
         // dd($request->all());
         $client = new \GuzzleHttp\Client();
         $url = "http://store.getnord.live/index.php?route=product/product/stock";
+        $cookieJar = CookieJar::fromArray([
+            'language' => $request->input('lang')
+        ], 'store.getnord.live');
 
         $response = $client->request('POST', $url, [
-            'form_params' => $request->all()
+            'form_params' => $request->all(),
+            'cookies' => $cookieJar
         ]);
         // $response = $client->post($url,  ['from_params'=> $request->all()]);
     

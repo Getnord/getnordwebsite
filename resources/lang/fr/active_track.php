561a07ce52336b4a9f7b7d0a4e36ba6146e1d756
Avetis90 <avetis.ananyan@gmail.com>
2020-04-22 15:49:21 +0400

    translations for track page

---
+++ resources/lang/fr/active_track.php
@@ -0,0 +1,90 @@
+<?php
+
+return [
+    'bannerTitle' => '<span>Getnord</span> Active Track',
+    'bannerText' => 'Guard Tour System.<span class="line_break"></span>
+                        Track your guards/personell<span class="line_break"></span>
+                        in nearly any business<span class="line_break"></span>
+                        environment.',
+    'productTitle' => 'Rugged Waterproof Security phone',
+    'productProcessor' => 'IP68 and MIL-STD-810 rated',
+    'productScreen' => 'A screen that\'s the thickest on the market',
+    'productBattery' => 'Extra long lasting 6000mAh battery ',
+    'productCharge' => 'Wireless charging',
+    'monitoringTitle' => 'Guard Tour System.<span class="line_break"></span>
+                            Lone Worker Monitoring',
+    'monitoringItems' => [
+        ['img' => 'nfc.svg', 'text' => 'NFC'],
+        ['img' => 'gsm.svg', 'text' => 'GSM/GPRS/LTE network - 2G / 3G / 4G'],
+        ['img' => 'battery.svg', 'text' => 'Rechargable (battery) <span class="line_break"></span><span class="lowercase">- approx. every 48 hrs</span>'],
+        ['img' => 'panic.svg', 'text' => 'Panic function'],
+        ['img' => 'two-way.svg', 'text' => 'Two-way voice communication <span class="line_break"></span><span class="lowercase">(telephone function)</span>'],
+        ['img' => 'man-down.svg', 'text' => 'Man-down function'],
+        ['img' => 'complete.svg', 'text' => 'Complete solution'],
+        ['img' => 'platform.svg', 'text' => 'Cloud based platform / possible <span class="line_break"></span>local access',
+            'subItems' => true,
+            'subItem1Text' => 'ebs <span class="line_break"></span><span class="font-small">activeview</span>',
+            'subItem2Text' => 'ebs <span class="line_break"></span><span class="font-small">activeview ENTERPRISE</span>'],
+        ['img' => 'user_friendly.svg', 'text' => 'Advanced and user friendly <span class="line_break"></span><span class="lowercase">reporting system</span>'],
+        ['img' => 'automatic.svg', 'text' => 'Automatic reports and <span class="line_break"></span>
+                                                information<span class="lowercase"> for decision makers,</span>
+                                                <span class="line_break"></span><span class="lowercase">supervisors and security personnel</span>'],
+        ['img' => 'range.svg', 'text' => 'Full range
+                                            <span class="line_break"></span><span class="lowercase">of the end user terminals </span>'],
+        ['img' => 'integrate.svg', 'text' => 'Easy to integrate
+                                            <span class="line_break"></span><span class="lowercase">with other systems</span>'],
+    ],
+    'section4Title' => 'Where can it be Used',
+    'section4Security' => 'SECURITY GUARD PATROL',
+    'section4Housekeeping' => 'Hotel and hospital housekeeping',
+    'section4Cleaning' => 'CLEANING SERVICES',
+    'section4Equipment' => 'EQUIPMENT MAINTENANCE ',
+    'section4Fleet' => 'FLEET CONTROL ',
+    'section4Delivery' => 'DELIVERY COURIERS',
+    'section4Postage' => 'POSTAGE ROUTES',
+    'section4Lawn' => 'LAWN SERVICES ROUTES ',
+    'section4Parking' => 'PARKING INSPECTORS ROUTES ',
+
+
+    'softwareTitle' => 'Software',
+    'softwareSubtitle' => 'Active View offers unlimited control over employees and protected objects from any place in the world to be done in <span class="line_break"></span> real time. All you need is access to the internet.',
+    'softwareActiveViewTitle' => 'ebs ACTIVE VIEW',
+    'softwareActiveViewImg' => 'ebs_activeview2.svg',
+    'softwareActiveViewItems' => [
+        ['img' => 'gsm-battery.svg', 'text' => 'Online monitoring of connection <span class="line_break"></span>status, battery level and GSM signal<span class="line_break"></span>strength'],
+        ['img' => 'database.svg', 'text' => 'Database of employees on duty'],
+        ['img' => 'preview-map.svg', 'text' => 'Current and historical preview <span class="line_break"></span>on the map of device’s position <span class="line_break"></span>(date, time, speed, direction, height)'],
+        ['img' => 'time-control.svg', 'text' => 'Work time control'],
+        ['img' => 'reporting-system.svg', 'text' => 'Advanced reporting system in .csv <span class="line_break"></span>format, SMS and Email notification'],
+        ['img' => 'route-notification.svg', 'text' => 'New route notification'],
+        ['img' => 'track-devices.svg', 'text' => 'Many Active Track devices <span class="line_break"></span>on one map'],
+        ['img' => 'geoTagging.svg', 'text' => 'GeoTagging'],
+    ],
+    'serverImg' => 'server.svg',
+    'serverTitle' => 'OSM. SERVER',
+    'serverItems' => [
+        ['img' => 'middleware-application.svg', 'text' => 'Middleware application whose<span class="line_break"></span>task is to safely receive, decrypt<span class="line_break"></span>and transmit signals from  devices<span class="line_break"></span>produced by EBS'],
+        ['img' => 'remote-programming.svg', 'text' => 'Remote programming, upgrade<span class="line_break"></span>and full, i.e. two-way integration<span class="line_break"></span>of EBS devices'],
+        ['img' => 'support-EBS.svg', 'text' => 'Support of all types of EBS devices'],
+        ['img' => 'database.svg', 'text' => 'Plain, simple multi tool for any<span class="line_break"></span>monitoring station'],
+    ],
+    'visionImg' => 'server.svg',
+    'visionTitle' => 'OSM. SERVER vision',
+    'visionItems' => [
+        ['img' => 'possibility.svg', 'text' => 'Possibility for the security agency <span class="line_break"></span>to store photos taken by pir cam'],
+        ['img' => 'preview-photos.svg', 'text' => 'Preview of photos from the camera<span class="line_break"></span>on the user’s smartphone or on<span class="line_break"></span>operator’s screen in the security<span class="line_break"></span>agency'],
+        ['img' => 'alarm.svg', 'text' => 'Video alarm verification'],
+        ['img' => 'ability.svg', 'text' => 'The ability to take photos on demand<span class="line_break"></span>by the end user and browse them<span class="line_break"></span>in the app'],
+        ['img' => 'opportunity.svg', 'text' => 'Opportunity to set up a server<span class="line_break"></span>at the client’s security agency –<span class="line_break"></span>photos will be managed on<span class="line_break"></span>their own resources'],
+    ],
+    'worksTitle' => 'How it works?',
+    'worksText1' => 'Getnord Active Track is a system based on Android Smartphone technologies for logging the rounds of employees in a variety of situations, for example: security guards patrolling property, technicians monitoring climate-controlled environments, and correctional officers checking prisoner living areas. It helps to ensure that the employee makes his or her appointed rounds at the correct intervals and can offer a record for legal or insurance reasons. ',
+    'worksText2' => 'Getnord Active Track can monitor the activities of patrol guards in real time, leaving little room for disobedience or straying from actual duty. Each guard or personnel member will be given a tool - i.e a smartphone installed with a Patrol App - and he must perform his round by tapping on the NFC tags or QR labels scattered along the assigned route. During a guard\'s patrol duty he can receive additional tasks from the control room; send notes and photos to report any incidences he observes in real time; and, in case of emergency, the mobile App is equipped with an SOS feature.',
+    'interestedTitle' => 'interested? wanna try?',
+    'interestedText' => 'Tell us about your business and we will arrange you a free sample of GETNORD phone with Enterprise version of GETNORD ACTIVE TACK preinstalled',
+    'interestedButtonText' => 'TRY GETNORD',
+
+    'lockTitle' => 'Worried<span class="line_break"></span> about improper usage of the Smartphone? ',
+    'lockText' => 'We have a solution which locks the phone and allows you to centrally control your employees phones. You can allow or restrict certain functions like apps, websites, downloads, internet connectivity, emails and other.',
+    'lockButtonText' => 'Read more about Getnord Lock',
+];

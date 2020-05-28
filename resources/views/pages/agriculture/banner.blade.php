2b469b48f310261852e3d3d3d45868a5e2f3b50c
Avetis90 <avetis.ananyan@gmail.com>
2020-05-28 23:19:18 +0400

    added banner video

--- resources/views/pages/agriculture/banner.blade.php
+++ resources/views/pages/agriculture/banner.blade.php
@@ -1,7 +1,9 @@
 <div id="track_banner" class="agriculture-banner">
     <div class="video_section">
-        <img src="{{asset('img/agriculture/banner-bg.png')}}" alt="">
-        <video src=""></video>
+        {{--<img src="{{asset('img/agriculture/banner-bg.png')}}" alt="">--}}
+        <video  autoplay loop muted playsinline inline>
+            <source src="{{asset('img/agriculture/agri_movie.mp4')}}" type="video/mp4">
+        </video>
     </div>
     <div class="banner_overlay">
         <div class="banner_text">
